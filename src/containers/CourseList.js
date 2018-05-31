ff9ff0d33817e0a72b53d628c5484333d06153e9
xiaodongm <39108568+xiaodongm@users.noreply.github.com>
2018-05-31 15:38:10 -0700

    In CourseList, findAllCourses() on mount

--- src/containers/CourseList.js
+++ src/containers/CourseList.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import CourseRow from '../components/CourseRow';
+import CourseService from '../services/CourseService';
+
 class CourseList extends React.Component {
     constructor() {
         super();
+        this.courseService = CourseService.instance;
+        this.state = {courses: []};
     }
-    render() {
+
+    componentDidMount() {
+        this.courseService.findAllCourses()
+            .then((courses) => {
+                this.setState({courses: courses});
+            });
+    }
+
+
+        render() {
         return (
             <div>
                 <h2>Course List</h2>
