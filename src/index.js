aec9181c22eddd95d14e362dac415962afe3161e
xiaodongm <39108568+xiaodongm@users.noreply.github.com>
2018-05-30 19:50:58 -0700

    refactor index.js to use the new container

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom'
+import CourseManager from './containers/CourseManager';
+
 
 ReactDOM.render(
-    <h1>Course Manager</h1>,
+    <CourseManager/>,
     document.getElementById('root')
 );
