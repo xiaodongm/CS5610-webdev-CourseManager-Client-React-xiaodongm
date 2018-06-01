04cacc948f7b6ad7998944d4b21d1f9168177247
xiaodongm <39108568+xiaodongm@users.noreply.github.com>
2018-05-31 23:00:43 -0700

    Created src/containers/CourseEditor.js

---
+++ src/containers/CourseEditor.js
@@ -0,0 +1,9 @@
+import React from 'react';
+class CourseEditor extends React.Component {
+    render() {
+        return (
+            <h3>Course Editor</h3>
+        )
+    }
+}
+export default CourseEditor;
