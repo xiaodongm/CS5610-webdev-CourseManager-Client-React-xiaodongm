a14abb015828a53e4779861ba09a42c50e3bd21b
xiaodongm <39108568+xiaodongm@users.noreply.github.com>
2018-06-01 16:03:17 -0700

    Added Create Module React Form in ModuleList.js

--- src/containers/ModuleList.js
+++ src/containers/ModuleList.js
@@ -1,13 +1,29 @@
 import React from 'react';
+import ModuleService from '../services/ModuleService';
+
 export default class ModuleList extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state = {courseId: ''};
+        this.state =
+            {courseId: '',
+            module: {title: ''}
+            };
         this.setCourseId = this.setCourseId.bind(this);
+        this.setModuleTitle = this.setModuleTitle.bind(this);
+        this.createModule = this.createModule.bind(this);
+
     }
     setCourseId(courseId) {
         this.setState({courseId: courseId});
+    }
+
+    setModuleTitle(event) {
+        this.setState({module: {title: event.target.value}})
+    }
+
+    createModule() {
+        ModuleService.createModule(this.state.courseId, this.state.module);
     }
 
     componentDidMount() {
@@ -20,7 +36,18 @@ export default class ModuleList extends React.Component {
 
     render() {
         return (
-            <h3>Module List for course: {this.state.courseId}</h3>
-        )
+            <div>
+                <h4>Module List for course: {this.state.courseId}</h4>
+                <input className="form-control"
+                       placeholder="New Module"
+                       value={this.state.module.title}
+                       onChange={this.setModuleTitle}/>
+                <button className="btn btn-primary btn-block"
+                        onClick={this.createModule}>
+                    <i className="fa fa-plus"></i>
+                </button>
+            </div>
+
+    )
     }
 }
