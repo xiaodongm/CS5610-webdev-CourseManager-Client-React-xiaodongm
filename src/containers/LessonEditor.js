743d9987c06056a6e94a0c541480aa3c9aa2574b
xiaodongm <39108568+xiaodongm@users.noreply.github.com>
2018-06-03 02:42:24 -0700

    Created LessonEditor.js

---
+++ src/containers/LessonEditor.js
@@ -0,0 +1,9 @@
+import React from 'react';
+export default class LessonEditor
+    extends React.Component {
+    render() {
+        return (
+            <h1>Lesson Editor</h1>
+        )
+    }
+}
