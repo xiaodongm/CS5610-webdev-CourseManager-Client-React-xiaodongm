51943fa289f86218a5676f25bebcaa638008ce6d
xiaodongm <39108568+xiaodongm@users.noreply.github.com>
2018-06-03 20:48:32 -0700

    added form to create new topic

--- src/containers/TopicList.js
+++ src/containers/TopicList.js
@@ -1,14 +1,35 @@
 import React from 'react';
+import TopicService from '../services/TopicService';
+
+
 export default class TopicList extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state = {lessonId: ''};
+        this.state = {lessonId: '',
+                      topic: {title: ''}};
         this.setLessonId = this.setLessonId.bind(this);
+        this.setTopicTitle = this.setTopicTitle.bind(this);
+        this.createTopic = this.createTopic.bind(this);
+
+
     }
     setLessonId(lessonId) {
         this.setState({lessonId: lessonId});
     }
+
+    setTopicTitle(event) {
+        this.setState({topic: {title: event.target.value}})
+    }
+
+    createTopic() {
+        TopicService
+            .createTopic(
+                this.state.lessonId,
+                this.state.topic);
+
+    }
+
 
 
     componentDidMount() {
@@ -23,7 +44,16 @@ export default class TopicList extends React.Component {
 
     render() {
         return (
-            <h4>{this.state.lessonId}</h4>
+            <div>
+                <h4>Topic List for lessonId:
+                    {this.state.lessonId}</h4>
+                <input placeholder="New Topic"
+                       value={this.state.lesson.title}
+                       onChange={this.setTopicTitle}
+                       onClick={this.createTopic}/>
+                <button>Create</button>
+            </div>
+
         )
     }
 }
